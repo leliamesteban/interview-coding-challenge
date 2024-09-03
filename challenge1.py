ead4970a300e54abe458e2a9f6a515459dd71f07
Liam Esteban Prince <liamesteban@gmail.com>
2024-09-03 11:00:44 +0200

    Create challenge1.py

---
+++ challenge1.py
@@ -0,0 +1,11 @@
+def challenge1(array1: array, array2: array, array3: array) -> array:
+  """
+  Given three ordered arrays of arbitrary length containing random capital letters, write an algorithm
+which returns the longest ordered array which all arrays share!
+Example
+Given the sequence ADDZ and CDDY and UDDF the longest shared array is DD.
+Example
+Given the sequence UIBAZDBSIAHFB, PQACIZDBIBDLAG and QIDBCZDBKSHDVF, the longest
+shared array is ZDB.
+  """
+  return []
